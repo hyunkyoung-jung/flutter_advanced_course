b555d524c3d4bb609c3ffeb40c3311a41de5b01f
hyunkyoung.jung <hyunkyoung.jung@kurlycorp.com>
2022-02-06 11:20:49 +0900

    add card theme at lecture 10

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/presentation/color_manager.dart';
+import 'package:flutter_advanced_course/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -10,9 +11,12 @@ ThemeData getApplicationTheme() {
       disabledColor: ColorManager.grey1,
       // will be used incase of disabled button for example
       colorScheme:
-          ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey)
+          ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey),
       // card view theme
-
+      cardTheme: CardTheme(
+          color: ColorManager.white,
+          shadowColor: ColorManager.grey,
+          elevation: AppSize.s4)
       // App bar theme
 
       // Button theme
