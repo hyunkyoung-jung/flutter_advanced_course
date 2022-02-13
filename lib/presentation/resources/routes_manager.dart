1ddbbe88d0abb832577f2d8ccb5d985fcc5a71ef
hyunkyoung.jung <hyunkyoung.jung@kurlycorp.com>
2022-02-13 10:59:44 +0900

    create routes manager at lecture 15

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,10 @@
+class Routes {
+  static const String splashRoute = "/";
+  static const String onBoardingRoute = "/onboarding";
+  static const String loginRoute = "/loginroute";
+  static const String registerRoute = "/register";
+  static const String forgotPasswordRoute = "/forgotPassword";
+  static const String mainRoute = "/main";
+  static const String storeDetailRoute = "/storeDetails";
+
+}
